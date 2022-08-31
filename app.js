abf01abaabc9d0cf915d3a0fe4e9aed0d0f28b93
Sardorbek Olimov <sardor.20070731@gmail.com>
2022-08-31 12:44:18 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,6 @@
+const nav = document.getElementById("nav");
+const navOpenBtn = document.getElementById("nav-open--btn");
+const navLinkP = document.querySelectorAll("#nav-link--p");
+const navLinkSvg = document.querySelectorAll(".nav_link svg");
+const brandIcon = document.getElementById("brand-icon");
+const navBrandText = document.getElementById("nav-brand-text");
