b16cb3e7e4d2a00b8056789e5a68c09d2b834090
Sardorbek Olimov <sardor.20070731@gmail.com>
2022-09-03 00:51:13 +0500

    message fixed card finished

--- js/app.js
+++ js/app.js
@@ -14,8 +14,7 @@ const messageOpen = document.getElementById("message-open");
 const messageClose = document.getElementById("message-close");
 const messageItemText = document.getElementById("message-item-text");
 const formCreate = document.getElementById("form-create");
-const createPost = document.getElementById("create-post");
-const responsePost = document.getElementById("response-post");
+const messageBody = document.getElementById("message-body");
 const questionInput = document.getElementById("question-input");
 
 function time() {
@@ -25,8 +24,7 @@ function time() {
   let minute =
     now.getMinutes() < 10 ? "0" + now.getMinutes() : now.getMinutes();
 
-  let year =
-    now.getFullYear() < 10 ? "0" + now.getFullYear() : now.getFullYear();
+  let year = now.getFullYear();
   let month = now.getMonth() < 10 ? "0" + (now.getMonth() + 1) : now.getMonth();
   let date = now.getDate() < 10 ? "0" + now.getDate() : now.getDate();
 
@@ -41,73 +39,61 @@ messageClose.addEventListener("click", () => {
 });
 
 formCreate.addEventListener("submit", (e) => {
+  let regEx = /^[a-zA-Z]$/;
   e.preventDefault();
-  createPost.innerHTML += `
-            <div class="flex align_center post_item">
-                <div class="post_date">
-                  <h4>${time()}</h4>
-                </div>
-                <div class="post_items flex">
+
+  if (questionInput.value <= 0) {
+    messageBody = false;
+  } else {
+    messageBody.innerHTML += `
+          <div class="flex align_center post_item wf-100">
+
+              <div class="post_date">
+                <h4>${time()}</h4>
+              </div>
+
+              <div class="post_items flex wf-100">
+
                   <div class="message_text quation_text">
                     <p>
-                      ${questionInput.value}
+                        ${questionInput.value}
                     </p>
                   </div>
-                  <div class="message_body_brand request_icon">
-                    <i class="fa-solid fa-user"></i>
-                  </div>
-                </div>
-            </div>`;
 
-  if (
-    questionInput.value === "Salom" ||
-    questionInput.value === "Assalomu alaykum"
-  ) {
-    setTimeout(() => {
-      responsePost.innerHTML += `
-              <div class="first_post post_item flex align_center">
-                  <div class="first_post_date post_date">
-                    <h4>${time()}</h4>
+                  <div class="message_body_brand request_icon">
+                      <i class="fa-solid fa-user"></i>
                   </div>
-                  <div class="post_items flex">
-                    <div class="message_body_brand message_brand">
-                      <i class="fa-solid fa-laptop" id="brand-icon"></i>
-                    </div>
-                    <div class="message_text">
-                      <p>
-                        Valaykum, assalom.<br />
-                        Hhurmatli mijoz! Siz tushinmagan<br />
-                        qismlaringiz bo'lsa savolingizni be-<br />
-                        rishingiz mumkin.<br />
-                      </p>
-                    </div>
-                  </div>
+
               </div>
-      `;
-    }, 2000);
-    createPost == createPost ? responsePost : createPost;
-  } else {
+          </div>`;
     setTimeout(() => {
-      responsePost.innerHTML += `
-                <div class="first_post post_item flex align_center">
-                    <div class="first_post_date post_date">
-                      <h4>${time()}</h4>
-                    </div>
-                    <div class="post_items flex">
-                      <div class="message_body_brand message_brand">
-                        <i class="fa-solid fa-laptop" id="brand-icon"></i>
-                      </div>
-                      <div class="message_text">
-                        <p>
-                          Bizning adminimiz bilan bog'lanib<br>
-                          savollarga javob olishingiz mumkin<br><br>
-                          Tel: +998 94 117 3949
-                        </p>
-                      </div>
-                    </div>
-                </div>
-        `;
-      responsePost == responsePost ? createPost : responsePost;
+      messageBody.innerHTML += `
+          <div class="first_post post_item flex align_center wf-100">
+
+              <div class="first_post_date post_date">
+                  <h4>${time()}</h4>
+              </div>
+
+              <div class="post_items flex wf-100">
+
+                  <div class="message_body_brand message_brand">
+                      <i class="fa-solid fa-laptop" id="brand-icon"></i>
+                  </div>
+
+                  <div class="message_text">
+                      <p>
+                        Assalomu, alaykum.<br />
+                        Hurmatli mijoz! Savollarga javobni<br>
+                        adminimizdan olishingiz mumkin,<br>
+                        shuningdek ko'p so'raladigan sa-<br>
+                        vollar bo'limidan savolingizga ja-<br>
+                        vob topishingiz mumkin.
+                      </p>
+                  </div>
+
+              </div>
+
+          </div>`;
     }, 2000);
   }
 
