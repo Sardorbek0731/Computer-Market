adbdb31ef723de2d80194a27a25dcba52101059e
Sardorbek Olimov <sardor.20070731@gmail.com>
2022-09-10 15:02:22 +0500

    added profil register

--- js/profil.js
+++ js/profil.js
@@ -1,1 +1,33 @@
-// Animate hover
+// Profil Register
+
+// Register Item
+const registerProfil = document.getElementById("register-profil");
+const firstName = document.getElementById("firstName");
+const lastName = document.getElementById("lastName");
+const email = document.getElementById("email");
+const age = document.getElementById("age");
+const message = document.getElementById("message");
+const openProfil = document.getElementById("open-profil");
+
+// Profil Item
+const profil = document.getElementById("profil");
+const userName = document.getElementById("user-name");
+const userEmail = document.getElementById("user-email");
+const userAbout = document.getElementById("user-about");
+const userAge = document.getElementById("user-age");
+const userYear = document.getElementById("user-year");
+
+let now = new Date();
+
+let year = now.getFullYear()
+
+openProfil.addEventListener("click", (e) => {
+  e.preventDefault();
+  registerProfil.style.display = "none";
+  profil.style.display = "block";
+  userName.innerHTML = `${firstName.value} ${lastName.value}`;
+  userEmail.innerHTML = email.value;
+  userAbout.innerHTML = message.value;
+  userYear.innerHTML = `${year - age.value} yil`;
+  userAge.innerHTML = age.value;
+});
