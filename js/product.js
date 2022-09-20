8a2fb3b3d4b3d57a62a71194b0284a3df6d54476
Sardorbek Olimov <sardor.20070731@gmail.com>
2022-09-20 19:37:58 +0500

    added image new name

--- js/product.js
+++ js/product.js
@@ -1,78 +1,108 @@
 // Product item
 const productMain = document.getElementById("product-main");
 const thereProductItems = document.getElementById("there-product-items");
-const thereProductItem = document.getElementById("there-product-item");
+const thereProductItem = document.getElementsByClassName("there_product_item");
+
+let counter = 0;
+let deleteCounter = 0;
+let productItemImages = [
+  "Image-1",
+  "Image-2",
+  "Image-3",
+  "Image-4",
+  "Image-5",
+  "Image-6",
+];
+let productImagesCounter = 0;
+console.log(productItemImages[productImagesCounter]);
+
+// There Product Item
 const thereProduct = document.getElementById("there-product");
-const topHeaderBag = document.getElementById("top-header-bag");
-const productBag = document.getElementById("product-bag");
+const emptyProduct = document.getElementById("empty-product");
+const placingAnOrderBtn = document.querySelectorAll("#placing-an-order-btn");
+console.log(placingAnOrderBtn);
 
-productBag.addEventListener("click", () => {
-  productMain.style.display = "none";
-  thereProduct.style.display = "block";
-  topHeaderBag.style.display = "block";
+placingAnOrderBtn.forEach((placingBtn) => {
+  placingBtn.addEventListener("click", () => {
+    thereProductItems.innerHTML += `
+      <div class="there_product_item align_center justify_evenly"
+          id=there-product-item>
+  
+            <div class="there_product_item_img">
+                  <img
+                    src="../image/Products/"
+                    alt="Dell image"
+                  />
+            </div>
+  
+            <div class="there_product_item_name flex justify_evenly">
+                  <h1>
+                    Dell XPS 17 (i9-12900HKRTX<br />
+                    3060)<br />
+                  </h1>
+                <div class="there_product_item_price">
+                    <h2>UZS 15,000,000</h2>
+                </div>
+            </div>
+  
+            <div class="there_product_item_date flex justify_evenly align_center" >
+                <div
+                    class="there_product_item_order_date there_product_item_date_title"
+                  >
+                    <h1>Buyurtma vaqti:</h1>
+                    <h3>14.09.2022</h3>
+                  </div>
+                  <div
+                    class="there_product_item_deliver_date there_product_item_date_title"
+                  >
+                    <h1>Yetkazish vaqti:</h1>
+                    <h3>18.09.2022</h3>
+                  </div>
+            </div>
+  
+            <div class="there_product_item_btns flex justify_evenly align_center" >
+  
+                <div class="there_product_item_delete there_product_item_btn">
+                    <button type="button" id="there-product-item-delete" onclick=deleteProduct(${counter})>
+                        <i class="fa-solid fa-trash-can"></i>
+                    </button>
+                </div>
+  
+                <div class="there_product_item_edit there_product_item_btn">
+                    <button type="button" id="there-product-item-edit">
+                      <i class="fa-solid fa-pen"></i>
+                    </button>
+                </div>
+  
+            </div>
+        </div>
+    `;
 
-  // There Product Item Delete and Edit
-
-  ProductItemDelete.addEventListener("click", () => {
-    thereProductItem.style.display = "none";
+    counter++;
   });
 });
 
-// There Product Item
-const placingAnOrderBtn = document.getElementById("placing-an-order-btn");
+const productBagLink = document.getElementById("product-bag-link");
+const topHeaderBag = document.getElementById("top-header-bag");
+const topHeader = document.getElementById("top-header");
 
-placingAnOrderBtn.addEventListener("click", () => {
-  thereProductItems.innerHTML += `
-    <div class="there_product_item flex align_center justify_evenly"
-        id="there-product-item">
+productBagLink.addEventListener("click", () => {
+  productMain.style.display = "none";
+  topHeader.style.display = "none";
+  topHeaderBag.style.display = "block";
 
-          <div class="there_product_item_img">
-                <img
-                  src="../image/Products/Dell XPS 17 (i9-12900HKRTX3060).png"
-                  alt="Dell image"
-                />
-          </div>
-
-          <div class="there_product_item_name flex justify_evenly">
-                <h1>
-                  Dell XPS 17 (i9-12900HKRTX<br />
-                  3060)<br />
-                </h1>
-              <div class="there_product_item_price">
-                  <h2>UZS 15,000,000</h2>
-              </div>
-          </div>
-
-          <div class="there_product_item_date flex justify_evenly align_center" >
-              <div
-                  class="there_product_item_order_date there_product_item_date_title"
-                >
-                  <h1>Buyurtma vaqti:</h1>
-                  <h3>14.09.2022</h3>
-                </div>
-                <div
-                  class="there_product_item_deliver_date there_product_item_date_title"
-                >
-                  <h1>Yetkazish vaqti:</h1>
-                  <h3>18.09.2022</h3>
-                </div>
-          </div>
-
-          <div class="there_product_item_btns flex justify_evenly align_center" >
-
-              <div class="there_product_item_delete there_product_item_btn">
-                  <button type="button" id=there-product-item-delete">
-                    <i class="fa-solid fa-trash-can"></i>
-                  </button>
-              </div>
-
-              <div class="there_product_item_edit there_product_item_btn">
-                  <button type="button" id="there-product-item-edit">
-                    <i class="fa-solid fa-pen"></i>
-                  </button>
-              </div>
-
-          </div>
-      </div>
-  `;
+  if (thereProductItem.length > 0) {
+    thereProduct.style.display = "block";
+  } else {
+    emptyProduct.style.display = "block";
+  }
 });
+
+function deleteProduct(counter) {
+  thereProductItem[counter].style.display = "none";
+  deleteCounter++;
+  if (thereProductItem.length === deleteCounter) {
+    thereProduct.style.display = "none";
+    emptyProduct.style.display = "block";
+  }
+}
