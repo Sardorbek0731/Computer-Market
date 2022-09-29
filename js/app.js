71359b736581f640bb3ac72fade7f86ba5cf3c8b
Sardorbek Olimov <sardor.20070731@gmail.com>
2022-09-30 00:20:02 +0500

    user-message updated

--- js/app.js
+++ js/app.js
@@ -8,7 +8,6 @@ setTimeout(() => {
   profilLoader.style.display = "none";
 }, 3000);
 
-
 // Message
 
 const messageOpen = document.getElementById("message-open");
@@ -16,7 +15,7 @@ const messageClose = document.getElementById("message-close");
 const messageItemText = document.getElementById("message-item-text");
 const formCreate = document.getElementById("form-create");
 const messageBody = document.getElementById("message-body");
-const questionInput = document.getElementById("question-input");
+const nameInput = document.getElementById("name-input");
 
 function time() {
   let now = new Date();
@@ -40,19 +39,17 @@ messageClose.addEventListener("click", () => {
 });
 
 formCreate.addEventListener("submit", (e) => {
-  let regEx = /^[a-zA-Z]$/;
   e.preventDefault();
-  let inputValue = questionInput.value;
+  let inputValue = nameInput.value;
   let uppercase = inputValue.charAt().toUpperCase();
   let lowercase = inputValue.slice(1).toLowerCase();
   let result = uppercase + lowercase;
 
-  if (result.length <= 0) {
-    messageBody = false;
-  } else if (
-    result === "Salom" ||
-    result === "Assalomu alaykum" ||
-    result === "Assalomu, alaykum"
+  if (
+    result.trim().length > 0 &&
+    nameInput.placeholder == `Ismingizni kiriting...` &&
+    result != "Salom" &&
+    result != "Assalomu alaykum"
   ) {
     messageBody.innerHTML += `
           <div class="flex align_center post_item wf-100">
@@ -75,7 +72,6 @@ formCreate.addEventListener("submit", (e) => {
 
               </div>
           </div>`;
-
     setTimeout(() => {
       messageBody.innerHTML += `
           <div class="first_post post_item flex align_center wf-100">
@@ -92,17 +88,19 @@ formCreate.addEventListener("submit", (e) => {
 
                   <div class="message_text">
                       <p>
-                        Valaykum, assalom.<br />
-                        Hurmatli mijoz, savolingiz bo'lsa<br />
-                        berishingiz mumkin!<br /> 
+                        Assalomu, alaykum! ${result}.<br />
+                        Hurmatli mijoz! Savollaringiz bo'lsa<br>
+                        berishingiz mumkin.<br>
                       </p>
                   </div>
 
               </div>
 
           </div>`;
+
+      nameInput.placeholder = `Savolingizni berishingiz mumkin...`;
     }, 2000);
-  } else {
+  } else if (result === "Salom" || result == "Assalomu alaykum") {
     messageBody.innerHTML += `
           <div class="flex align_center post_item wf-100">
 
@@ -140,13 +138,9 @@ formCreate.addEventListener("submit", (e) => {
 
                   <div class="message_text">
                       <p>
-                        Assalomu, alaykum.<br />
-                        Hurmatli mijoz! Savollarga javobni<br>
-                        adminimizdan olishingiz mumkin,<br>
-                        shuningdek ko'p so'raladigan sa-<br>
-                        vollar bo'limidan savolingizga ja-<br>
-                        vob topishingiz mumkin.<br><br>
-                        Admin tel: +998 94 117 3949
+                        Valaykum, alaykum!<br />
+                        Hurmatli mijoz! Savollaringiz bo'lsa<br>
+                        berishingiz mumkin.<br>
                       </p>
                   </div>
 
@@ -154,6 +148,69 @@ formCreate.addEventListener("submit", (e) => {
 
           </div>`;
     }, 2000);
+  } else if (
+    result.trim().length > 0 &&
+    nameInput.placeholder == `Savolingizni berishingiz mumkin...` &&
+    result != "Salom" &&
+    result != "Assalomu alaykum"
+  ) {
+    let questionInputValue = nameInput.value;
+    let qusetionUppercase = questionInputValue.charAt().toUpperCase();
+    let qusetionLowercase = questionInputValue.slice(1).toLowerCase();
+    let questionResult = qusetionUppercase + qusetionLowercase;
+
+    if (questionInputValue.trim().length > 0) {
+      messageBody.innerHTML += `
+          <div class="flex align_center post_item wf-100">
+
+              <div class="post_date">
+                <h4>${time()}</h4>
+              </div>
+
+              <div class="post_items flex wf-100">
+
+                  <div class="message_text quation_text">
+                    <p>
+                        ${questionResult}
+                    </p>
+                  </div>
+
+                  <div class="message_body_brand request_icon">
+                      <i class="fa-solid fa-user"></i>
+                  </div>
+
+              </div>
+          </div>`;
+      setTimeout(() => {
+        messageBody.innerHTML += `
+          <div class="first_post post_item flex align_center wf-100">
+
+              <div class="first_post_date post_date">
+                  <h4>${time()}</h4>
+              </div>
+
+              <div class="post_items flex wf-100">
+
+                  <div class="message_body_brand message_brand">
+                      <i class="fa-solid fa-laptop" id="brand-icon"></i>
+                  </div>
+
+                  <div class="message_text">
+                      <p>
+                        ${questionResult}: Bu savolingizga bizning admin
+                        bilan bog'lanish orqali olishingiz,
+                        shuningdek ko'p so'raladigan
+                        savollar bo'limdan ham savolingizga
+                        javob topishingiz mumkin.<br><br>
+                        Admin tel: +998 94 117 3949
+                      </p>
+                  </div>
+
+              </div>
+
+          </div>`;
+      }, 2000);
+    }
   }
 
   formCreate.reset();
