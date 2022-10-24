a86243d5791ca12330632746df32b8d59486e172
Sardorbek Olimov <sardor.20070731@gmail.com>
2022-10-24 22:54:11 +0500

    chenged advantage item

---
+++ js/questions.js
@@ -0,0 +1,7 @@
+const itemQuestions = document.getElementsByClassName("item_question");
+const answerDowns = document.getElementsByClassName("fa-angle-down");
+function angleDown(index) {
+  console.log(index, itemQuestions[index]);
+  itemQuestions[index].classList.toggle("click_angle_down");
+    answerDowns[index].classList.toggle("fa-angle-up");
+}
