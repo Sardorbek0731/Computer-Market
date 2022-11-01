09c2e5816028883526cc0c4faa51b7f2790071a4
Sardorbek Olimov <sardor.20070731@gmail.com>
2022-11-01 22:12:05 +0500

    The font-size in the setting has been fixed

--- js/setting.js
+++ js/setting.js
@@ -1,1 +1,35 @@
+// Global
+const fontSize = document.getElementById("font-size");
+const html = document.querySelector(`html`);
 
+let settingStorage = JSON.parse(localStorage.getItem("settings"))
+  ? JSON.parse(localStorage.getItem("settings"))
+  : [];
+
+// setItem
+function setItem() {
+  localStorage.setItem("settings", JSON.stringify(settingStorage));
+}
+
+// getItem
+function getItem() {
+  let settingItems = JSON.parse(localStorage.getItem("settings"));
+
+  settingItems.forEach((item) => {
+    console.log(item.fontSize);
+    html.style.fontSize = `${item.fontSize}px`;
+  });
+}
+
+function settingFunc() {
+  if (fontSize.value.length) {
+    settingStorage.push({
+      fontSize: fontSize.value,
+    });
+
+    setItem();
+    getItem();
+  }
+}
+
+if (settingStorage.length) getItem();
