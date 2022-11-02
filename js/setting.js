a6e22afe8d7abd9ac9aa98bdfc00c7bc936152e2
Sardorbek Olimov <sardor.20070731@gmail.com>
2022-11-02 21:30:56 +0500

    added html files setting.js

--- js/setting.js
+++ js/setting.js
@@ -2,6 +2,14 @@
 const fontSize = document.getElementById("font-size");
 const html = document.querySelector(`html`);
 
+if (window.screen.width >= 1600) {
+  fontSize.placeholder = `20`;
+} else if (window.screen.width >= 1440 && window.screen.width <= 1600) {
+  fontSize.placeholder = `16`;
+} else if (window.screen.width >= 1024 && window.screen.width <= 1440) {
+  fontSize.placeholder = `12`;
+}
+
 let settingStorage = JSON.parse(localStorage.getItem("settings"))
   ? JSON.parse(localStorage.getItem("settings"))
   : [];
